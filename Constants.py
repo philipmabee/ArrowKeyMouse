a730157adf42f34dbde1a4599a5af1be909d964a
AttilaB-7299 <attilabloom7299@gmail.com>
2026-05-17 23:36:30 -0400

    added more smooth mouse movements wheil keeping constants and toggle

--- Constants.py
+++ Constants.py
@@ -1,25 +1,19 @@
+from pynput import keyboard
 from Xlib import XK
-
-UP_KEY = "up"
-DOWN_KEY = "down"
-LEFT_KEY = "left"
-RIGHT_KEY = "right"
  
-LEFT_CLICK = "page_up"    # Numpad 9
-RIGHT_CLICK = "page_down"   # Numpad 3
+UP_KEY = keyboard.Key.up
+DOWN_KEY = keyboard.Key.down
+LEFT_KEY = keyboard.Key.left
+RIGHT_KEY = keyboard.Key.right
  
-TOGGLE_KEY = "tab"   # Change this to whatever key you want
-
-
-KEY_MAP = {
-    "up":       XK.XK_Up,
-    "down":     XK.XK_Down,
-    "left":     XK.XK_Left,
-    "right":    XK.XK_Right,
-    "page up":  XK.XK_Page_Up,
-    "page down":XK.XK_Page_Down,
-    "9":        XK.XK_KP_9,
-    "3":        XK.XK_KP_3,
-    "f8":       XK.XK_F8,
-    "escape":   XK.XK_Escape,
-}
+LEFT_CLICK = keyboard.Key.page_up
+RIGHT_CLICK = keyboard.Key.page_down
+ 
+TOGGLE_KEY = keyboard.Key.alt_l  # Change this to whatever key you want
+ 
+# Xlib keysyms for suppression (must match the keys above)
+SUPPRESSED_KEYSYMS = [
+    XK.XK_Up, XK.XK_Down, XK.XK_Left, XK.XK_Right,
+    XK.XK_Page_Up, XK.XK_Page_Down,
+]
+ 
